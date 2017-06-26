d3423006a5f9ffe9940dc769dd127bde7c64aef2
rhodesiax <rhodesiax.lo@gmail.com>
2017-06-26 15:41:01 +0800

    logic

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request,make_response,url_for,redirect
-from flask import  _app_ctx_stack
+from flask import session
+from flask import  _app_ctx_stack,g
 import os
 import sqlite3
 
@@ -21,28 +22,43 @@ def get_db():
     # if sqlite3 connection not exists,create one
     if not hasattr(top, 'sqlite3'):
         top.sqlite3 = sqlite3.connect(os.path.join(app.root_path, app.config['DATABASE']))
-        top.sqlite_db.row_factory = sqlite3.Row
-    return top.sqlite3
+        top.sqlite3.row_factory = sqlite3.Row
+    return top.sqlite3;
 
 # route
 @app.route('/')
 def show_entries():
-    return render_template('show_entries.html')
+    records = g.db.execute('select * from entries where 1').fetchall();
+    return render_template('show_entries.html',entries=records)
 
-@app.route('/addentry', methods=['POST'])
+@app.route('/add_entry', methods=['POST'])
 def add_entry():
-    pass
+    g.db.execute('insert into entries (title,text) values (?,?)',[request.form['title'],request.form['text']])
+    g.db.commit()
+    return redirect(url_for('show_entries'))
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if 'islogin' in session:
+        return redirect(url_for('show_entries'))
     if request.method == "POST":
-        pass
+        if request.form['username'] == 'admin' and request.form['password'] == 'admin':
+            # login success, write session
+            session['username'] = request.form['username']
+            session['islogin'] = True
+            return redirect(url_for('show_entries'))
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
     # clear session
+    session.pop('username',None)
+    session.pop('islogin', None)
     return redirect(url_for('show_entries'))
+
+@app.route('/signin',methods=['GET','POST'])
+def signin():
+    return 'Signin'
 
 @app.teardown_appcontext
 def close_conn(exception):
@@ -55,9 +71,6 @@ def before_requre():
     # get db
     g.db = get_db()
 
-@app.after_request
-def after_requre():
-    pass
 
 # entry point
 if __name__ == "__main__":
