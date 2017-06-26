bfd28e28135d3e32af76807848b722cda43c4fff
rhodesiax <rhodesiax.lo@gmail.com>
2017-06-26 14:51:08 +0800

    create pages

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,request,make_response,url_for,redirect
 
 
 # Flask object
@@ -7,7 +7,22 @@ app = Flask(__name__)
 # route
 @app.route('/')
 def show_entries():
-    return "show entries"
+    return render_template('show_entries.html')
+
+@app.route('/addentry', methods=['POST'])
+def add_entry():
+    pass
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == "POST":
+        pass
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    # clear session
+    return redirect(url_for('show_entries'))
 
 # entry point
 if __name__ == "__main__":
